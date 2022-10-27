3499afacf7b0198765cd9ddf3632310eb0f33c35
MIRIAN GEORGINA PINTO WELCHEZ <mirianpw1@unitec.edu>
2022-10-26 18:50:06 -0600

    POR FIN

---
+++ Conexion.h
@@ -0,0 +1,11 @@
+#pragma once
+
+using namespace std;
+using namespace System::Data;
+using namespace System;
+using namespace Sql;
+
+ref class Conexion
+{
+};
+
--- Form1.h
+++ Form1.h
@@ -10,7 +10,7 @@ namespace CppCLRWinFormsProject {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	using namespace System::Data::SqlClient;
+	using namespace System::Data::Odbc;
 	
 	
 
@@ -40,6 +40,11 @@ namespace CppCLRWinFormsProject {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	protected:
 
 	private:
@@ -56,36 +61,156 @@ namespace CppCLRWinFormsProject {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(304, 274);
+			this->button1->AccessibleName = L"";
+			this->button1->Cursor = System::Windows::Forms::Cursors::Default;
+			this->button1->Location = System::Drawing::Point(300, 385);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Iniciar";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(264, 270);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(155, 22);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(299, 220);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(76, 16);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Contraseña";
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(312, 101);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(54, 16);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Usuario";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(264, 137);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(155, 22);
+			this->textBox2->TabIndex = 4;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(12, 26);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(703, 343);
+			this->dataGridView1->TabIndex = 5;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(727, 501);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->Text = L"Usuario";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 
+		
+
+		String^ connString = "Dsn=TBD1;uid=Admin-Mirian";
+		OdbcConnection^ CON = gcnew OdbcConnection("Dsn=TBD1;uid=Admin-Mirian");
+
+		void ConnectionDB()
+		{
+			try
+			{
+				CON->Open();
+				OdbcCommand^ cmd = CON->CreateCommand();
+				cmd->CommandType = CommandType::Text;
+				cmd->CommandText = "Select * from Estudiante";
+				cmd->ExecuteNonQuery();
+
+				DataTable^ dt = gcnew DataTable();
+				OdbcDataAdapter^ dp = gcnew OdbcDataAdapter(cmd);
+				dp->Fill(dt);
+				dataGridView1->DataSource = dt;
+				CON->Close();
+
+				
+				
+				MessageBox::Show("Connection successful", "C++ Access Database Connector", MessageBoxButtons::OK, MessageBoxIcon::Error);
+
+
+
+			}
+			catch (Exception^ ex)
+			{
+				MessageBox::Show(ex->Message, "C++ Access Database Connector", MessageBoxButtons::OK, MessageBoxIcon::Error);
+
+			}
+		}
 
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		
+		ConnectionDB();
+
+		/*String^ Ic = this->textBox1->Text;
+
+		String^ connString = "Dsn=TBD1;uid=Admin-Mirian";
+		OdbcConnection^ CON = gcnew OdbcConnection("Dsn=TBD1;uid=Admin-Mirian");
+
+		try
+		{
+			CON->Open();
+			MessageBox::Show("Connection successful", "C++ Access Database Connector", MessageBoxButtons::OK, MessageBoxIcon::Error);
+
+
+
+		}
+		catch (Exception^ ex)
+		{
+			MessageBox::Show(ex->Message, "C++ Access Database Connector", MessageBoxButtons::OK, MessageBoxIcon::Error);
+
+		}*/
 		
 
 	}
-	};
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
